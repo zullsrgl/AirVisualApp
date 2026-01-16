db14f498c76450a84c09dd5889d0335744ee4f1e
zullsrgl <zulal.sariogluu@gmail.com>
2026-01-16 14:48:53 +0300

    Initial Commit

---
+++ AirVisualAppDemo/AirVisualAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirVisualAppDemoApp.swift
+//  AirVisualAppDemo
+//
+//  Created by Enes Arabacı on 16.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct AirVisualAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirVisualAppDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirVisualAppDemo
+//
+//  Created by Enes Arabacı on 16.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
